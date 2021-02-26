4f8485fbc31454854f19c65ee6a6890312961245
Fratchelya <70166836+Fratchelya@users.noreply.github.com>
2021-02-26 11:27:10 -0500

    Update Red to green.py

--- Red to green.py
+++ Red to green.py
@@ -4,9 +4,6 @@
 #This program transitions the color of a turtle from red to green 
 
 import turtle
-wn=turtle.Screen()
-wn.screensize(8000,500)
-#Import the turtle drawing package
 
 turtle.colormode(255)		#Allows colors to be given as 0...255
 tess = turtle.Turtle()		#Create a turtle
@@ -18,5 +15,5 @@ tess.backward(100)			#Move her backwards, to give more space to draw
 for i in range(0,255,10):
      tess.forward(25)		#Move forward
      tess.pensize(i)		#Set the drawing size to be i (larger each time)
-     tess.color(0,i,0)
      tess.color(255-i,i,0)
+
